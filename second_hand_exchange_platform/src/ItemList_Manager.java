0cc96f7705eae515924f044fb2aedc5eb1846408
Ashley <118498016+ashleychen128@users.noreply.github.com>
2024-05-30 19:35:37 +0800

    renrew

--- second_hand_exchange_platform/src/ItemList_Manager.java
+++
@@ -1,7 +0,0 @@
-
-public interface ItemList_Manager {
-	ItemList getList();
-	void addList(Item i);
-	Item getItem(int ID);
-	void deleteItem(Item i);
-}
--- second_hand_exchange_platform/src/Item_Manager.java
+++
@@ -1,8 +0,0 @@
-
-public interface Item_Manager {
-	String getIName();
-	int getID();
-	String getPName();
-	int getCondition();
-	String getDescription();
-}
